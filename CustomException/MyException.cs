1088c910f3869caa28868eaa8c649b3d1bf5bec3
Chinenye <c.okeke@genesystechhub.com>
2022-11-24 12:35:20 +0100

    Added more features

--- CustomException/MyException.cs
+++ CustomException/MyException.cs
@@ -5,10 +5,20 @@ namespace CustomException
     {
         private int _Age;
         private int _Experience;
+        private double _Level;
 
         private string _messageDetails = String.Empty;
-        public DateTime ErrorTimeStamp { get; set; }
-        public string CauseOfError { get; set; }
+        public DateTime ErrorTimeStamp {
+            get { 
+                return DateTime.Now; 
+            }
+        }
+        public string CauseOfError {
+            get {
+                return "The error is because the age or experience you entered is not above 1";
+            } 
+        
+        }
 
         public MyException() 
         {
@@ -28,27 +38,36 @@ namespace CustomException
         public MyException(string cause, DateTime time, string message, System.
         Exception inner) : base(message, inner)
         {
-            CauseOfError = cause;
-            ErrorTimeStamp = time;
+            
 
         }
 
         public void ExperienceCheck()
         {
-            Console.WriteLine("Enter Age: ");
-            _Age = Convert.ToInt32(Console.ReadLine());  
-
-            Console.WriteLine("Enter experience: ");
-            _Experience= Convert.ToInt32(Console.ReadLine());
-
-            if(_Age <= 0 ) 
-            { 
-            throw new MyException("Invalid Age Input",
-            DateTime.Now, $"{_Age} is an invalid age!")
+            try
             {
-                HelpLink = "http://www.stackoverflow.com",
-            };
+                Console.WriteLine("Enter Age: ");
+                _Age = Convert.ToInt32(Console.ReadLine());
 
+                Console.WriteLine("Enter experience: ");
+                _Experience= Convert.ToInt32(Console.ReadLine());
+
+
+                if (_Age <= 0 || _Experience < 0)
+                {
+                    throw new MyException("Invalid Age or Experience Input",
+                    DateTime.Now, $"{_Age} is an invalid age!");
+                }
+                else
+                {
+                    _Level = (_Experience / _Age) * 100;
+                    Console.WriteLine("Your level is {0}", _Level);
+                }
+            }
+            catch(Exception) 
+            {
+                Console.WriteLine("Cause of Error: {0}", CauseOfError);
+                Console.WriteLine("Time: {0}", ErrorTimeStamp);
             }
         }
     }
